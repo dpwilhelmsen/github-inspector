030d9a497e98d548d6f57587a98917bbd500e6bb
Daniel Wilhelmsen <dpwilhelmsen@yahoo.com>
2016-03-05 01:58:55 -0500

    Use the github url parsing library to parse search queries.
    The parse-github-urls library is integrated into the searchbox
    directive. It attempts to parse the query to find the owner and repo.
    If it doesn't match the expected format, it triggers an error which is
    displayed using the angular-bootstrap tooltip directive.

--- app/components/searchbox/searchbox-directive.js
+++ app/components/searchbox/searchbox-directive.js
@@ -1,12 +1,22 @@
 'use strict';
 
-angular.module('githubInspector.searchbox-directive', [])
-    .directive('searchbox', function() {
+angular.module('githubInspector.searchbox-directive', ['ui.bootstrap'])
+    .directive('searchbox', function($window) {
+        var gh = $window.parseGithub;
         return {
             controller: function () {
-                this.name = '';
+                this.repo = '';
+                this.error = "Couldn't find the repo. Please check your query and try again.";
+                this.open= false;
+                this.toggleTooltip = function(action) {
+                    this.open = !this.open;
+                }
                 this.submit = function() {
-                    alert(this.name);
+                    var query = gh(this.repo);
+                    if(!query.repository)
+                        return this.toggleTooltip();
+
+                    //Query Github
                 }
             },
             controllerAs: 'search',
